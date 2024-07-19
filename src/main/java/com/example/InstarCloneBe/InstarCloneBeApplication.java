1dfb8f4e168b0bc34b7e6064bbd0b97e65f6f6a7
codms0201 <chanyeol0615@naver.com>
2024-07-19 16:59:15 +0900

    Initial commit

---
+++ src/main/java/com/example/InstarCloneBe/InstarCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.InstarCloneBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstarCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstarCloneBeApplication.class, args);
+	}
+
+}
